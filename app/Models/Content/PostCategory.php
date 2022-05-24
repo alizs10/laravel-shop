0989da78daab984203e156313341b1674ced9553
Ali <ali.text77@gmail.com>
2022-05-24 14:15:50 +0430

    fix bug

--- app/Models/Content/PostCategory.php
+++ app/Models/Content/PostCategory.php
@@ -23,10 +23,4 @@ class PostCategory extends Model
     protected $fillable = ['name', 'slug', 'description', 'image', 'status', 'tags'];
 
     protected $casts = ['image' => 'array'];
-
-    
-    public function parent()
-    {
-        return $this->belongsTo($this, 'parent_id');
-    }
 }

50b506f5e7f7c9d0519b8fa824878e9c62efb67f
Ali Z.Soleimani <ali.text77@gmail.com>
2022-07-09 04:09:14 +0430

    add icon to menus migration

--- database/migrations/2021_12_20_161233_create_menus_table.php
+++ database/migrations/2021_12_20_161233_create_menus_table.php
@@ -16,6 +16,7 @@ class CreateMenusTable extends Migration
         Schema::create('menus', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('icon')->nullable();
             $table->string('url');
             $table->tinyInteger('status')->default(0);
             $table->foreignId('parent_id')->nullable()->constrained('menus')->onUpdate('cascade')->onDelete('cascade');

decc3ee8397657d79f037bbc766fa2f1b53ad082
Ali Z.Soleimani <ali.text77@gmail.com>
2022-07-17 08:19:24 +0430

    update: now ticket page in dynamic

--- resources/views/app/ticket.blade.php
+++ resources/views/app/ticket.blade.php
@@ -23,45 +23,67 @@
             <div class="flex justify-between gap-x-8 items-start">
                 <span class="flex gap-x-2 items-start text-lg text-gray-500 dark:text-gray-400">
                     <i class="fa-solid fa-tag mt-1"></i>
-                    <span>مشکل در سبد خرید</span>
+                    <span>{{ $ticket->subject }}</span>
                 </span>
                 <a href="" class="mt-1 text-gray-500 dark:text-gray-400 text-xs xs:text-base md:text-xs">
                     <i class="fa-regular fa-trash-alt"></i>
                 </a>
             </div>
 
-            <div class="flex flex-wrap gap-2 pb-2 border-b border-gray-400 dark:border-gray-700">
-                <span class="text-xxs bg-gray-200 dark:bg-gray-700 p-2 rounded-lg w-fit">۱۲ اسفند
-                    ۱۴۰۰</span>
-                <span class="text-xxs bg-gray-200 dark:bg-gray-700 p-2 rounded-lg w-fit">مهم</span>
-                <span class="text-xxs bg-gray-200 dark:bg-gray-700 p-2 rounded-lg w-fit">در انتظار
-                    پاسخ</span>
+            <div class="flex flex-wrap gap-2">
+                <span class="text-xxs bg-gray-200 dark:bg-gray-700 p-2 rounded-lg w-fit">
+                    {{ e2p_numbers(showPersianDate($ticket->create_at)) }}
+                </span>
+                <span
+                    class="text-xxs bg-gray-200 dark:bg-gray-700 p-2 rounded-lg w-fit">{{ $ticket->priority->name }}</span>
+                <span class="text-xxs bg-gray-200 dark:bg-gray-700 p-2 rounded-lg w-fit">
+                    @if ($ticket->status == 1)
+                        بسته شده
+                    @else
+                        در انتظار پاسخ
+                    @endif
+                </span>
             </div>
 
             <p class="text-xs xs:text-sm text-justify leading-6 xs:leading-7">
-                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،
-                چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
-                نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته
-                حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد.
+                {{ $ticket->description }}
             </p>
 
-            <span class="text-xs bg-red-500 text-white p-2 rounded-lg flex gap-x-2 items-center md:w-fit">
-                <i class="fa-solid fa-reply text-base"></i>
-                <span>پاسخ ادمین</span>
-            </span>
-            <p class="text-xs xs:text-sm text-justify leading-6 xs:leading-7">
-                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،
-                چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
-                نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته
-                حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد.
-            </p>
-            <span
-                class="text-xs bg-white dark:bg-gray-700 text-emerald-600 dark:text-emerald-500 p-2 rounded-lg flex gap-x-2 items-center md:w-fit">
-                <span class="w-4 h-4 rounded-full bg-emerald-600 dark:bg-emerald-500 flex-center text-white">
-                    <i class="fa-regular fa-check text-sm"></i>
+            @if ($ticket->children->count() > 0)
+                @foreach ($ticket->children as $ticket_child)
+                    @if ($ticket->reference_id == $ticket_child->author_id)
+                        <span class="text-xs bg-red-500 text-white p-2 rounded-lg flex gap-x-2 items-center md:w-fit">
+                            <i class="fa-solid fa-reply text-base"></i>
+                            <span>پاسخ ادمین</span>
+                        </span>
+                    @else
+                        <span
+                            class="text-xs bg-gray-200 dark:bg-gray-700 p-2 rounded-lg flex gap-x-2 items-center md:w-fit">
+                            <i class="fa-solid fa-reply text-base"></i>
+                            <span>پاسخ شما</span>
+                        </span>
+                    @endif
+
+                    <p class="text-xs xs:text-sm text-justify leading-6 xs:leading-7">
+                        {{ $ticket_child->description }}
+                    </p>
+                @endforeach
+            @endif
+
+            @if ($ticket->status == 1)
+                <span
+                    class="text-xs bg-white dark:bg-gray-700 text-emerald-600 dark:text-emerald-500 p-2 rounded-lg flex gap-x-2 items-center md:w-fit">
+                    <span class="w-4 h-4 rounded-full bg-emerald-600 dark:bg-emerald-500 flex-center text-white">
+                        <i class="fa-regular fa-check text-sm"></i>
+                    </span>
+                    <span>تیکت بسته شد</span>
                 </span>
-                <span>تیکت بسته شد</span>
-            </span>
+            @else
+                <span class="text-xxs bg-gray-200 dark:bg-gray-700 p-2 rounded-lg md:w-fit">
+                    در انتظار پاسخ
+                </span>
+            @endif
+
         </div>
 
 
